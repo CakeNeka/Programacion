544d58e93102a46b8a34549afeb5c28318936398
Neka <mdelopez777@gmail.com>
2023-01-12 21:34:07 +0100

    Prueba graficos

--- Otros/Pruebas/002_proyecto2d/src/p1/AFrame.java
+++ Otros/Pruebas/002_proyecto2d/src/p1/AFrame.java
@@ -1,17 +1,47 @@
 package p1;
 
-import javax.swing.JFrame;
+import java.awt.*;
+import javax.swing.*;
 
 public class AFrame extends JFrame {
 
     public AFrame() {
-        super();
-        // Title and default close operation
-        setTitle("Busca Pepos :)");
+
+        this.setBounds(0, 0, 600, 400);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setResizable(false);
-        this.setVisible(true);
+        this.setLocationRelativeTo(null);
     }
 
+    public static void main(String[] args) {
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new AFrame().setVisible(true);
+            }
+        });
+    }
 
+    @Override
+    public void paint(Graphics g) {
+        super.paint(g);  //
+        g.setColor(Color.blue);
+        g.drawLine(10, 60, 30, 400);
+        
+        int[] vectorX = {70, 130, 70, 23};
+        int[] vectorY = {70, 130, 130, 23};
+        g.drawPolygon(vectorX, vectorY, 3);
+    }
+
+}
+
+class APanel extends JPanel {
+
+    APanel() {
+        this.setSize(400, 400);
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        g.drawString("Hola mundo", 10, 10);
+    }
 }
--- Otros/Pruebas/002_proyecto2d/src/p1/Main.java
+++ Otros/Pruebas/002_proyecto2d/src/p1/Main.java
@@ -2,8 +2,6 @@ package p1;
 
 public class Main {
     
-    public static void main(String[] args) {
-        new AFrame();
-    }
+ 
     
 }
