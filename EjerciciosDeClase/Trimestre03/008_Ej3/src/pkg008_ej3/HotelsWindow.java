030deaf168aa075b18f8cdf43c39a32721302b55
Neka <mdelopez777@gmail.com>
2023-04-27 22:30:37 +0200

    Ejercicio 3

--- EjerciciosDeClase/Trimestre03/008_Ej3/src/pkg008_ej3/HotelsWindow.java
+++ EjerciciosDeClase/Trimestre03/008_Ej3/src/pkg008_ej3/HotelsWindow.java
@@ -11,7 +11,7 @@ import java.awt.event.WindowListener;
  *
  * @author Diurno
  */
-public class HotelsWindow extends javax.swing.JFrame implements WindowListener {
+public class HotelsWindow extends javax.swing.JFrame  {
 
     /**
      * Creates new form HotelsWindow
@@ -150,38 +150,4 @@ public class HotelsWindow extends javax.swing.JFrame implements WindowListener {
     private javax.swing.JLabel jLabel3;
     // End of variables declaration//GEN-END:variables
 
-    @Override
-    public void windowOpened(WindowEvent e) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
-    }
-
-    @Override
-    public void windowClosing(WindowEvent e) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
-    }
-
-    @Override
-    public void windowClosed(WindowEvent e) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
-    }
-
-    @Override
-    public void windowIconified(WindowEvent e) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
-    }
-
-    @Override
-    public void windowDeiconified(WindowEvent e) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
-    }
-
-    @Override
-    public void windowActivated(WindowEvent e) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
-    }
-
-    @Override
-    public void windowDeactivated(WindowEvent e) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
-    }
 }
